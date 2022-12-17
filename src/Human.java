114a2c8425c5530b91fefb9624b5b974ec4f6e19
Leodgar2010 <leodgar2010@yandex.ru>
2022-12-17 11:03:06 +0300

    Внес правки по замечаниям преподавателя

--- src/Human.java
+++ src/Human.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Human {
     String [] cloth;
@@ -9,6 +10,30 @@ public class Human {
         this.cloth = cloth;
         this.keys =keys;
         this.wishToTravel= wishToTravel;
+    }
+
+    public boolean tryToOpen (boolean open,boolean keys){
+        if (open || keys){
+            System.out.println("Шкаф открылся");
+            return true;
+
+        }else {
+            System.out.println("Шкаф закрыт");
+            return false;
+        }
+    }
+    public void travel(boolean roadToNarnia, boolean wishToTravel) {
+        if (roadToNarnia && wishToTravel) {
+            System.out.println("Добро пожаловать в Нарнию");
+        } else {
+            System.out.println("Через шкаф не попутешествовать");
+        }
+    }
+    public ArrayList<String> wearing(String[] contain, String[] wear) {
+        ArrayList <String> result = new ArrayList<>();
+        result.addAll(Arrays.asList(contain));
+        result.addAll(Arrays.asList(wear));
+        return  result;
     }
 
 }
--- src/Logic.java
+++ src/Logic.java
@@ -1,14 +1,13 @@
 public class Logic {
     Human human = new Human(true, true, new String[]{"Hat"});
     Wardrobe wardrobe = new Wardrobe("wood", false, true, new String[]{"Pants", "Shoes", "Shirt"});
-    Interaction interaction = new Interaction();
-    Logic () {}
+     Logic () {}
 
     public void game() {
         wardrobe.repairMeNow();
-        if (interaction.tryToOpen(human.keys, wardrobe.open)) {
-            System.out.println("Ваша одежда: " + interaction.wearing(human.cloth, wardrobe.contain));
-            interaction.travel(human.wishToTravel, wardrobe.roadToNarnia);
+        if (human.tryToOpen(human.keys, wardrobe.open)) {
+            System.out.println("Ваша одежда: " + human.wearing(human.cloth, wardrobe.contain));
+            human.travel(human.wishToTravel, wardrobe.roadToNarnia);
         } else {
             System.out.println("Попробуйте в следующий раз");
         }
